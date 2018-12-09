1fe64941a8ec87e1d34eefc8770d3c8ce9562e77
Balázs Suhajda <balazs.suhajda@cybercom.com>
2018-12-09 17:29:48 +0100

    Add files

---
+++ server.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/*
+	TODO: Complete README
+	TODO: Create example files for integration
+	TODO: Add screenshots
+ */
+const options = require('./options');
+require('carwings-mqtt-nodejs').setup(options);
